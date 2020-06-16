3cd31bc05f6bb2b3a32ecd6c3a3b12c1642cba75
r1oga <r1oga@tuta.io>
2020-06-16 22:46:35 +0200

    Watch files, rebuild and re run on change using Denon

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { desc, run, task } from 'https://deno.land/x/drake@v1.2.3/mod.ts'
-
-desc('Minimal Drake task')
-task('hello', [], function () {
-  console.log('Hello World!')
-})
-
-run()
+console.log('123')
